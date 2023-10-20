b4b9bff8ee8658269385ae2aebe1edbefd8b9f37
tubbadu <tubbadu@gmail.com>
2023-10-20 15:08:00 +0200

    added lab2

--- modopt-lab1/Lab2/Lab2/Exercise_4/proc.cpp
+++
@@ -1,6 +0,0 @@
-#include "proc.h"
-
-void Proc::proc_method(void) {
-	cout << "Proc object '" << name() << "' receives a value: " << in->read()  << " @ " << sc_time_stamp() << endl;
-	out->write(2*in->read());
-}
--- modopt-lab1/Lab2/Lab2/init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export SYSTEMC_HOME=/usr/local/systemc-2.3.3
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SYSTEMC_HOME/lib-linux64
